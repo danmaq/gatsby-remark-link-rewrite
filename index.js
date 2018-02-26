22f5dc9691fa505dc53db19d5942bea9f8e833c0
Shuhei Nomura <info@danmaq.com>
2018-02-26 09:42:44 +0900

    Add ESLint rule.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const visit = require('unist-util-visit');
 module.exports = ({ markdownAST }, pluginOptions) => {
   const defaults = { pattern: /^(.*)$/, replace: '$1' };
   const { pattern, replace } = Object.assign(defaults, pluginOptions);
-  
+
   /** @type {{(node: {url: string}) => void}} */
   const visitor = (node) => {
     node.url = node.url.replace(pattern, replace);
